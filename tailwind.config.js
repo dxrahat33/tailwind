08e993100dfb9f74e119cb943a36b6175381e38f
dxrahat33 <dxrahat33@gmail.com>
2024-08-15 08:58:31 +0600

    fist commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
